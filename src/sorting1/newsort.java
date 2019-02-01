2c753fd20d938d010b80a81d80e27a70bf9e343c
SA7136 <SA7136@CSELAB01-19.bennett.edu.in>
2019-02-01 09:31:51 +0530

    new sort

---
+++ src/sorting1/newsort.java
@@ -0,0 +1,18 @@
+package sorting1;
+
+import java.util.Arrays;
+
+public class newsort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int[] arr = { 13, 7, 6, 45, 21, 9, 101, 102 }; 
+		  
+	        Arrays.sort(arr); 
+	  
+	        System.out.printf("Modified arr[] : %s", 
+	                          Arrays.toString(arr)); 
+
+	}
+
+}
